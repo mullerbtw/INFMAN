583b78f34c2a6284a306f864680e2116949a2f1c
ismael müller <ismuller04@gmail.com>
2024-12-23 19:21:19 -0300

    gameplay improvements
    as of 23/12 afternoon.

--- gameplay.c
+++ gameplay.c
@@ -1,40 +1,71 @@
-#include <raylib.h>
-#include <raymath.h>
 #include <stdbool.h>
+#include "raylib.h"
+#include "raymath.h"
 
 int gameplay()
 {
 	const int screenWidth = 1200;
-    const int screenHeight = 600;
+    	const int screenHeight = 600;
     
 	InitWindow(screenWidth, screenHeight, "GAMEPLAY");
-	Texture2D megaman = LoadTexture("basicCharMovement.jpg");
-
-	// int frameWidth = megaman.width / 4;
+	Texture2D megaman = LoadTexture("charMoving.png");
     
-	Rectangle frameRec = {0.0f, 0.0f, (float)megaman.width / 4, (float)megaman.height};
-
-	Vector2 megamanPos = {(screenWidth / 2.0f) - ((255 / 4.0f) / 2), screenHeight / 2.0f};
-	
-    // Vector2 megamanPos = {screenWidth / 2, screenHeight / 2};
-
-    SetTargetFPS(60);
+    	unsigned numTiles = 3;
+    
+    	// int totalTileWidth = 74;
+    
+    	float megamanWidth = (float)megaman.width / numTiles;
+    
+	Rectangle frameRec = {0.0f, 0.0f, megamanWidth, (float)megaman.height};
+    
+	Vector2 megamanPos = {(screenWidth / 2.0f) - ((megaman.width / (float)numTiles) / 2), screenHeight / 2.0f};
+    
+	float megamanSpeed = 150;
+    
+    	SetTargetFPS(60);
 
 	while(!WindowShouldClose())
 	{
-		BeginDrawing();
+        Vector2 movement = {0, 0}; 
+		
+        BeginDrawing();
 
 		ClearBackground(WHITE);
 
 		DrawTextureRec(megaman, frameRec, megamanPos, WHITE); // background color is different from texture's white
-
-		// DrawTexture(megaman, screenWidth / 2 - megaman.width / 2, ScreenHeight / 2 - megaman.height / 2 - 40, RAYWHITE);
-
+        
+        if(IsKeyDown(KEY_W))
+        {
+            movement.y += -1;
+        }
+        
+        if(IsKeyDown(KEY_A))
+        {
+            movement.x += -1;
+            // frameRec.x -= (float)megamanWidth;
+            WaitTime(0.08);
+        }
+        
+        if(IsKeyDown(KEY_S))
+        {
+            movement.y += 1;
+        }
+        
+        if(IsKeyDown(KEY_D))
+        {
+            movement.x += 1;
+            frameRec.x += megamanWidth;
+            WaitTime(0.08);
+        }
+        
+        Vector2 movementScaling = Vector2Scale(movement, GetFrameTime() * megamanSpeed); // vetor para incrementar posição
+        
+        megamanPos = Vector2Add(megamanPos, movementScaling); // adds standard vector to the moving one
+        
 		EndDrawing();
 	}
 
 	CloseWindow();
 
 	return 0;
-    
 }
