3600fc5be07fd15c3e91da3e9dadb0b8744bce12
ismael müller <ismuller04@gmail.com>
2024-12-23 01:55:17 -0300

    corrected file name

--- binaryFileRead.c
+++ binaryFileRead.c
@@ -2,11 +2,10 @@ void binaryFileRead()
 {
     FILE *fp;
 
-    fp = fopen(nome_arq, "rb");
+    fp = fopen("top_scores.bin", "rb");
 
     if(fp != NULL)
     {
-        // fread(top5, sizeof(JOGADOR), MAXSCORES, fp);
         if(fread(leaderboard, sizeof(POSICOES), NUMJOG, fp) != NUMJOG)
             printf("ERRO NA LEITURA!");
         else
