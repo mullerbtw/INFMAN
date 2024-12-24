1ee66d791926a9e481d0de0f83673239741dc71b
ismael müller <ismuller04@gmail.com>
2024-12-23 23:51:21 -0300

    working movement
    movement is now working for both sides, with the tile also turning.

--- gameplay.c
+++ gameplay.c
@@ -5,67 +5,66 @@
 int main()
 {
 	const int screenWidth = 1200; // inicializa largura da janela
-    const int screenHeight = 600; // iniciliza altura da janela
+    	const int screenHeight = 600; // iniciliza altura da janela
     
-	InitWindow(screenWidth, screenHeight, "GAMEPLAY"); // iniciliza janela
+	InitWindow(screenWidth, screenHeight, "GAMEPLAY"); // inicializa janela
 	Texture2D megaman = LoadTexture("charMoving.png"); // carrega textura de movimento do megaman
     
-    unsigned numTiles = 3; // variável para armazenar número de figuras do megaman na charMoving.png
+    	unsigned numTiles = 3; // variável para armazenar número de figuras do megaman na charMoving.png
     
-    // bool inverted = false;
+    	bool megamanMovingRight = false;
+    	bool megamanMovingLeft = false;
     
-    float oneWidth = (float)megaman.width / numTiles; // variável para armazenar largura de um dos três desenhos
-        
+    	float oneWidth = (float)megaman.width / numTiles; // variável para armazenar largura de um dos três desenhos
+    
 	Rectangle frameRec = {0.0f, 0.0f, oneWidth, (float)megaman.height}; // cria retângulo onde fica megaman
 	Vector2 megamanPos = {(screenWidth / 2.0f) - ((oneWidth) / 2), screenHeight / 2.0f}; // vetor para posição do megaman
     
 	float megamanSpeed = 100; // variável que armazena velocidade (em pixels/second)
     
-    SetTargetFPS(60); // setando fps da janela do jogo
+    	SetTargetFPS(60); // setando fps da janela do jogo
 
 	while(!WindowShouldClose())
 	{
-        Vector2 movement = {0, 0};  // vetor de movimento (necessário pra fazer ele andar)
+        	Vector2 movement = {0, 0};  // vetor de movimento (necessário pra fazer ele andar)
 		
-        BeginDrawing();
+        	BeginDrawing();
 
 		ClearBackground(WHITE);
 
 		DrawTextureRec(megaman, frameRec, megamanPos, WHITE); // desenha textura do megaman
         
-        if(IsKeyDown(KEY_W)) // se W estiver apertado, vai pra cima
-        {
-            movement.y += -1;
-        }
+        	if(IsKeyDown(KEY_W)) // se W estiver apertado, vai pra cima
+        	{
+            		movement.y += -1;
+        	}
         
-        if(IsKeyDown(KEY_A)) // se A estiver apertado, vai pra trás
-        {   
-            // inverted = true;
-            // if(inverted == true)
-            megaman.width = -megaman.width;
-            movement.x += -1;
-            frameRec.x += oneWidth;
-            WaitTime(0.1);
-        }
+        	if(IsKeyDown(KEY_A)) // se A estiver apertado, vai pra trás
+        	{   
+            		if(frameRec.width > 0)
+                		frameRec.width = -frameRec.width;
+            		movement.x += -1;
+            		frameRec.x += oneWidth;
+            		WaitTime(0.1);
+        	}
         
-        if(IsKeyDown(KEY_S)) // se S estiver apertado, vai pra baixo
-        {
-            movement.y += 1;
-        }
+        	if(IsKeyDown(KEY_S)) // se S estiver apertado, vai pra baixo
+        	{
+            		movement.y += 1;
+        	}
         
-        if(IsKeyDown(KEY_D)) // se D estiver apertado, vai pra frente
-        {
-            // inverted = false;
-            // if(inverted == false)
-            megaman.width = megaman.width;
-            movement.x += 1;
-            frameRec.x += oneWidth;
-            WaitTime(0.1);
-        }
+        	if(IsKeyDown(KEY_D)) // se D estiver apertado, vai pra frente
+        	{
+            		if(frameRec.width < 0)
+                		frameRec.width = -frameRec.width;
+            		movement.x += 1;
+            		frameRec.x += oneWidth;
+            		WaitTime(0.1);
+        	}
         
-        // esses steps são necessários pra fazer ele andar
-        Vector2 movementScaling = Vector2Scale(movement, GetFrameTime() * megamanSpeed); // vetor para incrementar posição
-        megamanPos = Vector2Add(megamanPos, movementScaling); // adds standard vector to the moving one
+        	// esses steps são necessários pra fazer ele andar
+        	Vector2 movementScaling = Vector2Scale(movement, GetFrameTime() * megamanSpeed); // vetor para incrementar posição
+        	megamanPos = Vector2Add(megamanPos, movementScaling); // adds standard vector to the moving one
         
 		EndDrawing();
 	}
