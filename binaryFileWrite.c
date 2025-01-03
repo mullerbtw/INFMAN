2783c39bd076802732eeeaf8125fcfcf007541f6
ismael müller <ismuller04@gmail.com>
2025-01-03 15:38:38 -0300

    deleted binaryFileWrite.c

--- binaryFileWrite.c
+++
@@ -1,8 +0,0 @@
-void binaryFileWrite(int posicao, char nomeJog[], int pontuacao)
-{
-    POSICOES novoJog = {nomeJog, pontuacao};
-    
-    leaderboard[posicao] = novoJog;
-
-    printf("INFORMACAO TROCADA NO ARRAY!");
-}
